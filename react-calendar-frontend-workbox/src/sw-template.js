3f0da7d926c507d32c5e25b14130156306e5afa1
Hugo_Mendoza <hugo_mendoza_s@outlook.com>
2023-06-11 12:30:29 -0500

    feat: finish service worker section

--- react-calendar-frontend-workbox/src/sw-template.js
+++ react-calendar-frontend-workbox/src/sw-template.js
@@ -1,5 +1,88 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-// eslint-disable-next-line no-undef, no-restricted-globals
+workbox.loadModule("workbox-background-sync")
+
 workbox.precaching.precacheAndRoute( self.__WB_MANIFEST );
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
+
+const cacheNetworkFirst = [
+  "/api/auth/renew",
+  "/api/events"
+]
+
+registerRoute(
+  ({ request, url }) => {
+    // console.log({request, url});
+    if( cacheNetworkFirst.includes(url.pathname) ) return true;
+    return false;
+  },
+  new NetworkFirst()
+)
+
+const cacheFirstNetwork = [
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
+];
+
+registerRoute(
+  ({ request, url }) => {
+    if( cacheNetworkFirst.includes(url.href) ) return true;
+    return false;
+  },
+  new CacheFirst()
+)
+
+
+// registerRoute(
+//   new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
+//   new CacheFirst()
+// )
+
+// registerRoute(
+//   new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
+//   new CacheFirst()
+// )
+
+
+// registerRoute(
+//   new RegExp('http://localhost:4000/api/auth/renew'),
+//   new NetworkFirst()
+// )
+
+// registerRoute(
+//   new RegExp('http://localhost:4000/api/events'),
+//   new NetworkFirst()
+// )
+
+//** Posteos offline */
+const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
+  maxRetentionTime: 24 * 60
+});
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  "POST"
+)
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  "DELETE"
+)
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  "PUT"
+)
