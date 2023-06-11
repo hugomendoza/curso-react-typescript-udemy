e5f79510aec11475e5865e3be558af578456779c
Hugo_Mendoza <hugo_mendoza_s@outlook.com>
2023-06-11 13:49:13 -0500

    feat: init section maps

---
+++ 09-maps-app/src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { MapsApp } from './MapsApp';
+
+if( !navigator.geolocation ) {
+  alert("Geolocation is not supported by your browser");
+  throw new Error("Geolocation is not supported by your browser");
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <MapsApp />
+  </React.StrictMode>
+);
---
+++ 09-maps-app/src/screens/HomeScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MapView } from '../components'
+
+export const HomeScreen = () => {
+  return (
+    <MapView />
+  )
+}
